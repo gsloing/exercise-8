e7fa556ec02a82f79b0ec826219b0334947581bf
Devan Prabowo <s11810160@student.unklab.ac.id>
2023-10-18 17:04:38 +0800

    Menambahkan Fitur Card-info

---
+++ src/components/Card-info/index.jsx
@@ -0,0 +1,12 @@
+const CardInfo = () => {
+    return (
+    <>
+        <h2> Card Info </h2>
+        <Evolution />
+        <Logo />
+        <TypeEffect />
+    </>
+    )
+};
+
+export default CardInfo;
